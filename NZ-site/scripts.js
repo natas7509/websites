a8c031a9c6a4829cbffff39ff7239bf4cc37478d
natas7509 <42814959+natas7509@users.noreply.github.com>
2020-04-19 03:21:19 -0400

    hero complete

--- NZ-site/scripts.js
+++ NZ-site/scripts.js
@@ -1,1 +1,13 @@
-//start
+// Function to select elements
+
+const selectElement = (e) => document.querySelector(e);
+
+// Open the menu when clicked
+selectElement(".open").addEventListener("click", (e) => {
+  selectElement(".nav-list").classList.add("active");
+});
+
+// Close the menu when clicked
+selectElement(".close").addEventListener("click", (e) => {
+  selectElement(".nav-list").classList.remove("active");
+});
