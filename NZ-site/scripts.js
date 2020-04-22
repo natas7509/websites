a6fb3b3e2e5f3defe3f6e7a23b0532c7582fa983
natas7509 <42814959+natas7509@users.noreply.github.com>
2020-04-22 14:54:43 -0400

    close function for nav-links

--- NZ-site/scripts.js
+++ NZ-site/scripts.js
@@ -1,6 +1,7 @@
 // Function to select elements
 
 const selectElement = (e) => document.querySelector(e);
+const selectLink = (e) => document.querySelector(e);
 
 // Open the menu when clicked
 selectElement(".open").addEventListener("click", (e) => {
@@ -10,4 +11,9 @@ selectElement(".open").addEventListener("click", (e) => {
 // Close the menu when clicked
 selectElement(".close").addEventListener("click", (e) => {
   selectElement(".nav-list").classList.remove("active");
+});
+
+// Close menu when a nav link is clicked
+selectLink(".closeLink").addEventListener("click", (e) => {
+  selectLink(".nav-list").classList.remove("active");
 });
